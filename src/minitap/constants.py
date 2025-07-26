813205271e41b01d221dda17353edee04d88b0bd
Pierre-Louis <pierre-louis.favreau@epita.fr>
2025-07-26 14:07:13 +0200

    refactor: move constants to constants file

--- src/minitap/constants.py
+++ src/minitap/constants.py
@@ -1,3 +1,5 @@
+from minitap.context import LLMModel, LLMProvider
+
 EXPIRED_TOOL_MESSAGE = "(expired message, removed to save memory)"
 FORBIDDEN_MAESTRO_TOOLS = {
     "start_device",
@@ -18,3 +20,18 @@ HIDDEN_MAESTRO_TOOLS = {"inspect_view_hierarchy"}
 SCREENSHOT_LIFETIME = 3
 RECURSION_LIMIT = 400
 MAX_MESSAGES_IN_HISTORY = 25
+
+
+AVAILABLE_MODELS: dict[LLMProvider, list[LLMModel]] = {
+    "openai": ["o3"],
+    "google": ["gemini-2.5-pro"],
+    "openrouter": [
+        "moonshotai/kimi-k2",
+        "meta-llama/llama-4-maverick",
+        "meta-llama/llama-4-scout",
+    ],
+    "xai": ["grok-4"],
+}
+
+DEFAULT_PROVIDER: LLMProvider = "openai"
+DEFAULT_MODEL: LLMModel = "o3"
