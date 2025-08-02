f0c1d314ac2e519dc582aceab788e03affaa0b8a
Pierre-Louis <pierre-louis.favreau@epita.fr>
2025-08-02 19:13:18 +0200

    feat: add all the mcp commands + some useful ones

--- src/minitap/controllers/mobile_command_controller.py
+++ src/minitap/controllers/mobile_command_controller.py
@@ -1,4 +1,5 @@
-from typing import Optional
+from enum import Enum
+from typing import Literal, Optional
 
 from pydantic import BaseModel, Field
 
@@ -10,6 +11,9 @@ from minitap.utils.logger import get_logger
 screen_api = get_screen_api_client()
 device_hardware_api = get_device_hardware_client()
 logger = get_logger(__name__)
+
+
+###### Screen elements retrieval ######
 
 
 class ScreenDataResponse(BaseModel):
@@ -25,6 +29,11 @@ def get_screen_data():
     return ScreenDataResponse(**response.json())
 
 
+def take_screenshot():
+    return get_screen_data().base64
+
+
+###### Interacting with the screen #####
 class RunFlowRequest(BaseModel):
     yaml: str
     dry_run: bool = Field(default=False, alias="dryRun")
@@ -73,17 +82,24 @@ def format_selector_request_for_yaml(request: SelectorRequest):
     elif request.percentages:
         yaml += f"\n point: {request.percentages.x_percent}%,{request.percentages.y_percent}%\n"
     elif request.text:
-        yaml += f" {request.text}\n"
+        yaml += f"\n text: {request.text}\n"
     return yaml
 
 
-def tap(request: SelectorRequest):
+def tap(request: SelectorRequest, index: Optional[int] = None):
+    """
+    Tap on a selector.
+    Index is optional and is used when you have multiple views matching the same selector.
+    """
     yaml = format_selector_request_for_yaml(request)
     if not yaml:
         error = "Invalid tap selector request, could not format yaml"
         logger.error(error)
         raise ControllerErrors(error)
-    return run_flow(f"tapOn:{yaml}")
+    flow_input = f"tapOn:{yaml}"
+    if index:
+        flow_input += f"\nindex: {index}\n"
+    return run_flow(flow_input)
 
 
 def long_press_on(request: SelectorRequest):
@@ -95,8 +111,65 @@ def long_press_on(request: SelectorRequest):
     return run_flow(f"longPressOn:{yaml}")
 
 
-def back():
-    return run_flow("back\n", dry_run=True)
+class SwipeStartEndCoordinatesRequest(BaseModel):
+    start: CoordinatesSelectorRequest
+    end: CoordinatesSelectorRequest
+
+
+class SwipeStartEndPercentagesRequest(BaseModel):
+    start: PercentagesSelectorRequest
+    end: PercentagesSelectorRequest
+
+
+class SwipeRequest(BaseModel):
+    start_end_coordinates: Optional[SwipeStartEndCoordinatesRequest] = None
+    start_end_percentages: Optional[SwipeStartEndPercentagesRequest] = None
+    direction: Optional[Literal["UP", "DOWN", "LEFT", "RIGHT"]] = None
+    duration: Optional[int] = None  # in ms, default is 400ms
+
+
+def swipe(request: SwipeRequest):
+    yaml = ""
+    if request.start_end_coordinates:
+        yaml += f"\n start: {request.start_end_coordinates.start.x},"
+        yaml += f"{request.start_end_coordinates.start.y}\n"
+        yaml += (
+            f"\n end: {request.start_end_coordinates.end.x},{request.start_end_coordinates.end.y}\n"
+        )
+    elif request.start_end_percentages:
+        yaml += f"\n start: {request.start_end_percentages.start.x_percent}%,"
+        yaml += f"{request.start_end_percentages.start.y_percent}%\n"
+        yaml += f"\n end: {request.start_end_percentages.end.x_percent}%,"
+        yaml += f"{request.start_end_percentages.end.y_percent}%\n"
+    if request.direction:
+        yaml += f"\n direction: {request.direction}\n"
+    if request.duration:
+        yaml += f"\n duration: {request.duration}\n"
+    if not yaml:
+        error = "Invalid swipe selector request, could not format yaml"
+        logger.error(error)
+        raise ControllerErrors(error)
+    return run_flow(f"swipe:{yaml}")
+
+
+##### Text related commands #####
+
+
+def input_text(text: str):
+    return run_flow(f"inputText: {text}\n")
+
+
+def copy_text_from(request: SelectorRequest):
+    yaml = format_selector_request_for_yaml(request)
+    if not yaml:
+        error = "Invalid copy text from selector request, could not format yaml"
+        logger.error(error)
+        raise ControllerErrors(error)
+    return run_flow(f"copyTextFrom:{yaml}\n")
+
+
+def paste_text():
+    return run_flow("pasteText\n")
 
 
 def erase_text(nb_chars: Optional[int] = None):
@@ -109,5 +182,54 @@ def erase_text(nb_chars: Optional[int] = None):
     return run_flow(f"eraseText: {nb_chars}\n")
 
 
+##### App related commands #####
+
+
+def launch_app(package_name: str):
+    return run_flow(f"launchApp: {package_name}\n")
+
+
+def stop_app(package_name: str):
+    return run_flow(f"stopApp: {package_name}\n")
+
+
+##### Key related commands #####
+
+
+def back():
+    return run_flow("back\n")
+
+
+class Key(Enum):
+    ENTER = "Enter"
+    HOME = "Home"
+    BACK = "Back"
+
+
+def press_key(key: Key):
+    return run_flow(f"pressKey: {key.value}\n")
+
+
+#### Other commands ####
+
+
+class WaitTimeout(Enum):
+    SHORT = 500
+    MEDIUM = 1000
+    LONG = 5000
+
+
+def wait_for_animation_to_end(timeout: Optional[WaitTimeout] = None):
+    if timeout is None:
+        return run_flow("waitForAnimationToEnd\n")
+    return run_flow(f"waitForAnimationToEnd:\n  timeout: {timeout.value}\n")
+
+
 if __name__ == "__main__":
-    back()
+    launch_app("org.mozilla.firefox")
+    tap(SelectorRequest(text="Search or enter.*"))
+    input_text("tesla model s")
+    wait_for_animation_to_end()
+    press_key(Key.ENTER)
+    wait_for_animation_to_end()
+    print("done")
