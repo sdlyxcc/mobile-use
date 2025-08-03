8657529970cbf304c599d4b0ea7b371da8948bc4
Pierre-Louis <pierre-louis.favreau@epita.fr>
2025-08-03 16:45:33 +0200

    refactor: refactored state preparing new agentic architecture

--- src/minitap/graph/state.py
+++ src/minitap/graph/state.py
@@ -14,10 +14,32 @@ class Subgoal(BaseModel):
 
 
 class State(AgentStatePydantic):
+    # planner related keys
     initial_goal: Annotated[str, "Initial goal given by the user"]
+
+    # orchestrator related keys
     is_goal_achieved: Annotated[bool, "Whether the goal has been achieved"]
-    trace_id: Annotated[str | None, "ID of the run"]
     subgoal_history: Annotated[list[Subgoal], "History of subgoals"]
+
+    # contextor related keys
+    latest_screenshot_base64: Annotated[str | None, "Latest screenshot of the device"]
+    latest_ui_hierarchy: Annotated[Optional[str], "Latest UI hierarchy of the device"]
+    focused_app_info: Annotated[Optional[str], "Focused app info"]
+    screen_size: Annotated[Optional[str], "Screen size"]
+    device_date: Annotated[Optional[str], "Date of the device"]
+
+    # cortex related keys
+    structured_decisions: Annotated[
+        dict,
+        "Structured decisions made by the cortex, for the executor to follow",
+    ]
+
+    # common keys
     current_subgoal: Annotated[Optional[Subgoal], "Current subgoal"]
-    memory: Annotated[Optional[str], "Your long term memory"]
-    past_observations: Annotated[list[str], "Observations about the environment", add]
+    agents_thoughts: Annotated[
+        list[str],
+        "All thoughts and reasons that led to actions (why a tool was called, expected outcomes..)",
+        add,
+    ]
+    # extra keys
+    trace_id: Annotated[str | None, "ID of the run"]
