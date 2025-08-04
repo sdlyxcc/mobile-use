d034e32b70a8532628326a8d91fc0dde9beed4e4
Pierre-Louis <pierre-louis.favreau@epita.fr>
2025-08-04 09:14:50 +0200

    refactor: adding new executor tool and removing planning tools

--- src/minitap/tools/index.py
+++ src/minitap/tools/index.py
@@ -1,4 +1,3 @@
-# Mobile tools
 from minitap.tools.mobile.back import back_wrapper
 from minitap.tools.mobile.copy_text_from import copy_text_from_wrapper
 from minitap.tools.mobile.erase_text import erase_text_wrapper
@@ -6,6 +5,7 @@ from minitap.tools.mobile.input_text import input_text_wrapper
 from minitap.tools.mobile.launch_app import launch_app_wrapper
 from minitap.tools.mobile.list_packages import list_packages_wrapper
 from minitap.tools.mobile.long_press_on import long_press_on_wrapper
+from minitap.tools.mobile.open_link import open_link_wrapper
 from minitap.tools.mobile.paste_text import paste_text_wrapper
 from minitap.tools.mobile.press_key import press_key_wrapper
 from minitap.tools.mobile.run_flow import run_flow_wrapper
@@ -14,15 +14,11 @@ from minitap.tools.mobile.swipe import swipe_wrapper
 from minitap.tools.mobile.take_screenshot import take_screenshot_wrapper
 from minitap.tools.mobile.tap import tap_wrapper
 from minitap.tools.mobile.wait_for_animation_to_end import wait_for_animation_to_end_wrapper
-
-# Planning tools
-from minitap.tools.planning.complete_goal import complete_goal_wrapper
-from minitap.tools.planning.end_subgoal import end_subgoal_wrapper
-from minitap.tools.planning.start_subgoal import start_subgoal_wrapper
 from minitap.tools.tool_wrapper import ToolWrapper
 
 EXECUTOR_WRAPPERS_TOOLS = [
     back_wrapper,
+    open_link_wrapper,
     tap_wrapper,
     long_press_on_wrapper,
     swipe_wrapper,
@@ -39,12 +35,6 @@ EXECUTOR_WRAPPERS_TOOLS = [
     wait_for_animation_to_end_wrapper,
 ]
 
-PLANNING_WRAPPER_TOOLS = [
-    start_subgoal_wrapper,
-    end_subgoal_wrapper,
-    complete_goal_wrapper,
-]
-
 
 def get_tools_from_wrappers(wrappers: list[ToolWrapper]):
     """Get the tools from the wrappers."""
@@ -53,7 +43,7 @@ def get_tools_from_wrappers(wrappers: list[ToolWrapper]):
 
 def get_tool_wrapper_from_name(name: str) -> ToolWrapper | None:
     """Get the tool wrapper from the name."""
-    for wrapper in EXECUTOR_WRAPPERS_TOOLS + PLANNING_WRAPPER_TOOLS:
+    for wrapper in EXECUTOR_WRAPPERS_TOOLS:
         if wrapper.tool_fn.__name__ == name:
             return wrapper
     return None
