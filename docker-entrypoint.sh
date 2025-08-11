58f70dba51671ba87612e515eeeccd6c11cebc97
Jean-Pierre Lo <jeanpierre_lo@protonmail.com>
2025-08-11 20:23:57 +0200

    feat(docker-entrypoint): ADB connect is retried until it works

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,23 @@ set -eu
 trap 'kill $(jobs -p)' SIGTERM SIGINT
 
 if [ ! -z "${ADB_CONNECT_ADDR:-}" ]; then
-    adb connect "$ADB_CONNECT_ADDR"
+    while true; do
+        output="$(adb connect "$ADB_CONNECT_ADDR" 2>&1)"
+        exit_code=$?
+        echo $output
+
+        if [[ $exit_code -ne 0 ]]; then
+            echo "ADB connect failed with exit code $exit_code"
+        elif echo "$output" | grep -qi "unable to connect\|connection refused"; then
+            echo "Connection refused or unable to connect detected in ADB output"
+        else
+            echo "ADB connect successful"
+            break
+        fi
+
+        echo "ADB connect failed, retrying in 2s..."
+        sleep 2
+    done
 fi
 
 sleep 2
